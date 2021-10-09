c27fd5c497b2df2c96214a50b95af33bfb5eb1d2
Shrishti <66330519+shrishti-sisodiya@users.noreply.github.com>
2021-10-09 23:41:07 +0530

    Add files via upload

---
+++ Greaterno.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Greatestno
+{	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+   	System.out.println("Enter num1:");
+	int num1=sc.nextInt();
+	System.out.println("Enter num2:");
+   	int num2=sc.nextInt();
+   	System.out.println("Enter num3:");
+   	int num3=sc.nextInt();
+	int result;
+	if 
